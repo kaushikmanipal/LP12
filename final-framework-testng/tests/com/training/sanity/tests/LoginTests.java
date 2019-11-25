a51bb5f4ebcdb61550c6589b43ed4d1969a8607b
lenovo <lenovo@lenovo-t450>
2019-11-25 11:38:31 +0530

    Changed code to print out changed code agains the

--- final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
@@ -48,7 +48,7 @@ public class LoginTests {
 	@Test
 	public void validLoginTest() {
 		
-		System.out.println("Changed code");
+		System.out.println("Changed code again");
 		loginPOM.sendUserName("admin");
 		loginPOM.sendPassword("admin@123");
 		loginPOM.clickLoginBtn(); 
